09f280284c08876db614bad38dd725c1da85eb2e
omer-akkoca <omerakkoca1042@gmail.com>
2025-04-24 18:40:01 +0300

    navigation bar made transparent

--- app.tsx
+++ app.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider } from "./src/providers";
 import { Onboarding } from "./src/pages";
+import SystemNavigationBar from 'react-native-system-navigation-bar';
 
 const App = (): React.JSX.Element => {
+
+    useEffect(() => {
+        SystemNavigationBar.setNavigationColor('translucent');
+    }, [])
+
     return(
         <ThemeProvider>
             <Onboarding/>
