78bc3db384c54aff8bacc4fdf7e21a1136f9db8a
omer-akkoca <omerakkoca1042@gmail.com>
2025-04-25 05:20:59 +0300

    horizontal layout component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 import P from "./p.tsx"
 import CImage from "./image.tsx"
+import HorizontalLayout from "./horizontal_layout.tsx"
+import AppBar from "./app_bar.tsx"
 
-export { P, CImage };
+export { P, CImage, HorizontalLayout, AppBar };
