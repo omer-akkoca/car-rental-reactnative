803bd56279a88eed6094e652eb649ce9a40c0633
omer-akkoca <omerakkoca1042@gmail.com>
2025-04-29 13:49:17 +0300

    mapImage animation

--- src/pages/car_details.tsx
+++ src/pages/car_details.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import React, { useEffect } from "react";
+import { Animated, StyleSheet, TouchableOpacity, useAnimatedValue, View } from "react-native";
 import { AppBar, CImage, HorizontalLayout, P, Space } from "../components";
 import { NavigationProp, RootStackParamList } from "../types";
 import { useTheme } from "../providers";
@@ -14,14 +14,24 @@ const CarDetails = () => {
     const { colors } = useTheme()
     const { params: { car } } = useRoute<RouteProp<RootStackParamList, "car_details">>()
 
-    return(
+    const scaleAnim = useAnimatedValue(1);
+
+    useEffect(() => {
+        Animated.timing(scaleAnim, {
+            toValue: 1.5,
+            duration: 2000,
+            useNativeDriver: true,
+        }).start();
+    }, [])
+
+    return (
         <View style={{ flex: 1, backgroundColor: colors.pageBg }}>
             <AppBar
                 title="Information"
             />
-            <View style={{ flex: 1, }}>
+            <View style={{ flex: 1 }}>
                 <View style={{ padding: 16 }}>
-                    <CarCard car={car}/>
+                    <CarCard car={car} />
                 </View>
                 <HorizontalLayout customStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}>
                     <View style={[styles.userInfoContainer, shadowStyle, { backgroundColor: colors.white, shadowColor: colors.black }]}>
@@ -30,28 +40,30 @@ const CarDetails = () => {
                             width={80}
                             height={80}
                         />
-                        <Space height={10}/>
+                        <Space height={10} />
                         <P color="black" weight="bold" align="center">Jane Cooper</P>
                         <P color="grey">$4,253</P>
                     </View>
-                    <Space width={16}/>
+                    <Space width={16} />
                     <TouchableOpacity
                         activeOpacity={0.75}
                         style={[styles.mapsContainer, shadowStyle, { backgroundColor: colors.white, shadowColor: colors.black }]}
                         onPress={() => navigation.navigate("map_details", { car })}
                     >
-                        <CImage
-                            local={mapsImage}
-                            height={175}
-                            width={"100%"}
-                        />
+                        <Animated.View style={{ transform: [{ scale: scaleAnim }] }}>
+                            <CImage
+                                local={mapsImage}
+                                height={175}
+                                width={"100%"}
+                            />
+                        </Animated.View>
                     </TouchableOpacity>
                 </HorizontalLayout>
                 <View style={{ paddingHorizontal: 16, gap: 8 }}>
-                    <MoreCard car={car}/>
-                    <MoreCard car={car}/>
-                    <MoreCard car={car}/>
-                    <MoreCard car={car}/>
+                    <MoreCard car={car} />
+                    <MoreCard car={car} />
+                    <MoreCard car={car} />
+                    <MoreCard car={car} />
                 </View>
             </View>
         </View>
@@ -70,6 +82,7 @@ const styles = StyleSheet.create({
         flex: 1,
         height: 175,
         borderRadius: 20,
+        overflow: "hidden",
     }
 })
 
